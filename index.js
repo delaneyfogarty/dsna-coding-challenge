718c258d24de3616157e15a4817458d551fd732f
Beau Elliott <belliott1516@gmail.com>
2022-06-22 10:11:56 -0700

    added filter function to index.js

--- index.js
+++ index.js
@@ -13,3 +13,21 @@ function map(arr, callback) {
 
 map(myArray, (x) => x * 2);
 map(myArray, (x) => x ** 2);
+
+
+function filter(arr, callback){
+  const newArr = [];
+  for(let i=0; i < arr.length; i++) {
+    const number = arr[i];
+    const newItem = callback(number);
+    if (newItem){
+      newArr.push(number);
+    }
+    
+  }
+  console.log(newArr);
+    return newArr
+}
+
+filter([2, 6, 5], x => x % 2 === 0);
+filter([6, 17, 34], x => x % 17 === 0);

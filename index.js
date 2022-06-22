b2c500d230bbfea55868687452e09ed5adb927c5
Delaney Fogarty <delaney.fk@gmail.com>
2022-06-22 11:24:26 -0700

    every function

--- index.js
+++ index.js
@@ -30,6 +30,22 @@ function filter(arr, callback) {
 filter([2, 6, 5], (x) => x % 2 === 0);
 filter([6, 17, 34], (x) => x % 17 === 0);
 
+function every(arr, callback) {
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i];
+    if (callback(item)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+const variable = every(myArray, (x) => x % 2 === 0);
+const variable2 = every([4, 8, 12, 14], (x) => x % 2 === 0);
+console.log(variable2);
+console.log(variable);
+
 function addPunctuation(punctuation) {
   return (string) => {
     return string + punctuation;
@@ -39,14 +55,13 @@ function addPunctuation(punctuation) {
 const exclamation = addPunctuation("!!!")("hi");
 console.log(exclamation);
 
-
-function addFirst(element){
+function addFirst(element) {
   return (arr) => {
-    return [element, ...arr ];
-  }
+    return [element, ...arr];
+  };
 }
 
-const addStart = addFirst('purple')(['red', 'orange', 'blue']);
-console.log('addStart', addStart);
+const addStart = addFirst("purple")(["red", "orange", "blue"]);
+console.log("addStart", addStart);
 const addNumber = addFirst(1)([2, 3, 4, 5]);
-console.log('addNumber', addNumber);
+console.log("addNumber", addNumber);
